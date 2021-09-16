ba7c289a073872885bdada0e0209fc1632362aa5
jameskyboxa45 <88876153+jameskyboxa45@users.noreply.github.com>
2021-09-17 00:22:39 +0700

    Update group.php

--- group.php
+++ group.php
@@ -25,9 +25,9 @@ The above copyright notice and this permission notice shall be included in all c
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
-  <link href="../assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
+  <link href="./assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="../assets/demo/demo.css" rel="stylesheet" />
+  <link href="./assets/demo/demo.css" rel="stylesheet" />
 </head>
 
 <body class="">
@@ -44,7 +44,7 @@ The above copyright notice and this permission notice shall be included in all c
       <div class="sidebar-wrapper">
         <ul class="nav">
           <li class="nav-item ">
-            <a class="nav-link" href="./dashboard.html">
+            <a class="nav-link" href="./index.html">
               <i class="material-icons">dashboard</i>
               <p>Dashboard</p>
             </a>
@@ -56,7 +56,7 @@ The above copyright notice and this permission notice shall be included in all c
             </a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="./tables.html">
+            <a class="nav-link" href="./group.html">
               <i class="material-icons">content_paste</i>
               <p>Manager </p>
             </a>
